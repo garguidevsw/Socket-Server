9154bd70d370b714f877018ab55ec709b3ad8f59
JCGARGUI <garguidevsw@gmail.com>
2018-12-08 18:31:31 -0600

    Primer commit - Rest Server

---
+++ README.md
@@ -0,0 +1,17 @@
+# Socket - Server
+
+Reconstruir módulos de Node
+```
+npm install
+```
+
+Generar el Dist
+```
+tsc -w
+```
+
+Levantar el servidor, cualquiera de los dos comandos
+```
+nodemon dist/
+node dist/
+```
---
+++ index.ts
@@ -0,0 +1,24 @@
+import Server from "./classes/server";
+import { SERVER_PORT } from './global/environment';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+const server = new Server();
+
+//Configuración BodyParser
+
+server.app.use( bodyParser.urlencoded({ extended: true }) );
+server.app.use( bodyParser.json() );
+
+// Configuración CORS
+server.app.use( cors({ origin: true, credentials: true }) );
+
+//Rutas de Servicio
+
+server.app.use('/', router);
+
+server.start( () => {
+    console.log(`Servidor corriendo en el puerto ${ SERVER_PORT }`);
+});
